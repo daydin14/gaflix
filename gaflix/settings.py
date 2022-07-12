ebe65f48c551558960a56d72a8f111d40b87ec8d
David Aydin <david.aydin6@gmail.com>
2022-07-12 00:02:29 -0400

    Setup done

--- gaflix/settings.py
+++ gaflix/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'gaflix.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'gaflix',
     }
 }
 
