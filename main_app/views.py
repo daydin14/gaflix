23cabce72128327c0b969db6feaf58d2e5291c0f
David Aydin <david.aydin6@gmail.com>
2022-07-12 01:15:42 -0400

    Template Inheritance and Static Files within

--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('<h1>Hello World!</h1>')
+    return render(request, 'home.html')
 
 def resources(request):
     return render(request, 'resources.html')
