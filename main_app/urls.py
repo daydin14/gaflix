380904184aca84a41b9af3d5a0fce70073b5c34b
David Aydin <david.aydin6@gmail.com>
2022-07-13 13:58:10 -0400

    Read in CRUD done for resources

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,13 +4,15 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     
-    path('resources', views.resources, name='resources'),
+    path('resources/', views.resourceIndex, name='resource_list'),
     
-    path('course-details', views.course_details, name='course-details'),
+    path('resources/<int:resource_id>', views.resourceDetail, name='resource_detail'),
     
-    path('class-material', views.class_material, name='class-material'),
+    path('course-details/', views.course_details, name='course-details'),
+    
+    path('class-material/', views.class_material, name='class-material'),
 
-    path('login', views.login, name='login'),
+    path('login/', views.login, name='login'),
     
-    path('signup', views.signup, name='signup')
+    path('signup/', views.signup, name='signup')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -5,9 +5,13 @@ from .models import Resources
 def home(request):
     return render(request, 'home.html')
 
-def resources(request):
+def resourceIndex(request):
     resources = Resources.objects.all()
     return render(request, 'resources.html', {'resources': resources} )
+
+def resourceDetail(request, resource_id):
+    resources = Resources.objects.get(id=resource_id)
+    return render(request, 'resource-details.html', {'resources':resources})
 
 def login(request):
     return render(request, 'login.html')
