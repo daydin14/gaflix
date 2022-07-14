055f58b3d118c6dcb09eaf3ad71e877696e8bea8
David Aydin <david.aydin6@gmail.com>
2022-07-14 10:08:34 -0400

    Moved django_heroku import at bottom of settings.py

--- gaflix/settings.py
+++ gaflix/settings.py
@@ -13,8 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 from decouple import config
-import django_heroku
-django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,9 +121,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-LOGIN_REDIRECT_URL = '/resources/'
+LOGIN_REDIRECT_URL = '/course-details/'
 LOGOUT_REDIRECT_URL = '/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+django_heroku.settings(locals())
